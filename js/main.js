6453ec5b23c46049a0e09c97dded3f1580e05707
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-02-17 15:02:54 +0600

    added threejs

--- js/main.js
+++ js/main.js
@@ -10,14 +10,15 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
 
 camera.position.z = 5;
 function animate() {
   renderer.render(scene, camera);
+  // cube.rotation.x += 0.01;
   requestAnimationFrame(animate);
 }
 animate();
